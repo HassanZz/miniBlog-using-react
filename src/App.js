95a89476c4c83da67e9998119359534772670484
HassanZz <hassan.zamani.75@gmail.com>
2022-05-10 14:24:23 +0430

    some dynamic variables have added to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 
 function App() {
+  const title = "Wellcome to this blog";
+  const likes = 50;
+  const link = "http://www.google.com";
+  //  const person = { name: "hassan", age: 25 };
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> */}
+
+        <p>
+          {10}
+          {" hello world"}
+          {[1, 2, 43, 4, 56]}
+        </p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}> Google</a>
       </div>
     </div>
   );
