c38baca856677aed9d9a9d8a9120d49f27571a81
HassanZz <hassan.zamani.75@gmail.com>
2022-05-10 14:50:16 +0430

    all variable removed. 'Home' and 'Navbar' components imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Wellcome to this blog";
-  const likes = 50;
-  const link = "http://www.google.com";
-  //  const person = { name: "hassan", age: 25 };
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
-
-        <p>
-          {10}
-          {" hello world"}
-          {[1, 2, 43, 4, 56]}
-        </p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}> Google</a>
+        <Home></Home>
       </div>
     </div>
   );
