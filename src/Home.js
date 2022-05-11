72b7a09b21a02fa2bd964dbcef6b90f3b686723c
HassanZz <hassan.zamani.75@gmail.com>
2022-05-11 11:49:13 +0430

    event listener for clickme buttom added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("hellowww");
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello" + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain(" hassan", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
