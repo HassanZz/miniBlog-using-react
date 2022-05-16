b47c42634766e36a13afabe589590058d41df97e
HassanZz <hassan.zamani.75@gmail.com>
2022-05-16 21:23:06 +0430

    another hook method is added: useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./bloglist";
 
 const Home = () => {
@@ -16,13 +16,26 @@ const Home = () => {
       id: 2,
     },
   ]);
+  const [name, setName] = useState("hassan");
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button
+        onClick={() => {
+          setName("patrick");
+        }}
+      >
+        Change name
+      </button>
+      <p>{name}</p>
       {/* <BlogList
       blogs={blogs.filter((item) => item.author === "hassan")}
       title="Hassan's blogs"/> */}
