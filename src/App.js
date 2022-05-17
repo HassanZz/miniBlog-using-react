ab28379d4e4aff6eb98a46af3ef63b9d7c964b85
HassanZz <hassan.zamani.75@gmail.com>
2022-05-17 13:29:19 +0430

    router added to project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <Home></Home>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
