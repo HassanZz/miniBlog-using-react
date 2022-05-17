41262216504a54e2631d5708448e36595e59ac7a
HassanZz <hassan.zamani.75@gmail.com>
2022-05-17 12:37:58 +0430

    error messages of fetching and dont existing data added

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,29 @@ import BlogList from "./bloglist";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data!");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
   return (
     <div className="home">
+      {error && <div> {error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
