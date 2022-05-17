1c5b3c8f1805068047152692b5e3bc893af6a9ec
HassanZz <hassan.zamani.75@gmail.com>
2022-05-17 22:31:14 +0430

    abrotadded to prevent errors of fetching data from db

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,8 +4,11 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+
+    fetch(url, { signal: abortCont.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error("could not fetch the data!");
@@ -18,10 +21,15 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
       });
-  }, []);
+    return () => abortCont.abort();
+  }, [url]);
   return { data, isPending, error };
 };
 
