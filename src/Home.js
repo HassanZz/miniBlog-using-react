0b302ff1ae6743390db790d230b30485cc6cdb34
HassanZz <hassan.zamani.75@gmail.com>
2022-05-17 12:09:04 +0430

    conditional template added

--- src/Home.js
+++ src/Home.js
@@ -2,43 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./bloglist";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "my new website",
-      body: "lorem ipsum....",
-      author: "hassan",
-      id: 1,
-    },
-    {
-      title: "Welcome to my website",
-      body: "lorem ipsum....",
-      author: "eli",
-      id: 2,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
   const [name, setName] = useState("hassan");
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
 
   useEffect(() => {
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button
-        onClick={() => {
-          setName("patrick");
-        }}
-      >
-        Change name
-      </button>
-      <p>{name}</p>
-      {/* <BlogList
-      blogs={blogs.filter((item) => item.author === "hassan")}
-      title="Hassan's blogs"/> */}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
