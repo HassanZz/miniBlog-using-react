aca72400e896ea6376d9accca2ca6fc6e981fbc2
HassanZz <hassan.zamani.75@gmail.com>
2022-05-17 12:54:06 +0430

    a custom hook added to be reusable

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,12 @@
-import { useState, useEffect } from "react";
 import BlogList from "./bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch the data!");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
       {error && <div> {error}</div>}
