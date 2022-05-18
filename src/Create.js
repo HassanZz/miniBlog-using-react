9650b23479d47368e6d8273ddb279f41e8cb2ace
HassanZz <hassan.zamani.75@gmail.com>
2022-05-18 13:55:59 +0430

    initial setup of add blog added

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("hassan");
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="hassan">hassan</option>
+          <option value="eli">eli</option>
+        </select>
+        <button>Add</button>
+      </form>
     </div>
   );
 };
